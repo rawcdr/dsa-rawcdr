dac0e4153a30a6fc7c3daebebf2f2cf07a716b73
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-07-03 00:19:51 +0530

    Time: 0 ms (100%), Space: 46.9 MB (25.41%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,28 @@
+class Solution {
+    int dr[] = {-1, 1, 0, 0};
+    int dc[] = {0, 0, -1, 1};
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int org = image[sr][sc];
+        if(org == color) return image;
+
+        dfs(image, sr, sc, org, color);
+        return image;
+    }
+
+    private void dfs(int[][] image, int row, int col, int org, int color){
+        image[row][col] = color;
+
+        int m = image.length;
+        int n = image[0].length;
+
+        for(int i=0;i<4;i++){
+            int nr = row + dr[i];
+            int nc = col + dc[i];
+
+            if(nr >= 0 && nc >= 0 && nr < m && nc < n && image[nr][nc] == org){
+                dfs(image, nr, nc, org, color);
+            }
+        }
+    }
+}
