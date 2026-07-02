3e5aaaa0ffb6bf489536aced71ce549d79a3313e
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-07-03 00:30:41 +0530

    Time: 0 ms (100%), Space: 46.9 MB (25.41%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,8 +4,7 @@ class Solution {
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int org = image[sr][sc];
-        if(org == color) return image;
-
+        if(org == color) return image;;
         dfs(image, sr, sc, org, color);
         return image;
     }
@@ -14,13 +13,13 @@ class Solution {
         image[row][col] = color;
 
         int m = image.length;
-        int n = image[0].length;
+        int n = image[0] .length;
 
         for(int i=0;i<4;i++){
             int nr = row + dr[i];
             int nc = col + dc[i];
 
-            if(nr >= 0 && nc >= 0 && nr < m && nc < n && image[nr][nc] == org){
+            if(nr >= 0 && nr < m && nc >= 0 && nc < n && image[nr][nc] == org){
                 dfs(image, nr, nc, org, color);
             }
         }
