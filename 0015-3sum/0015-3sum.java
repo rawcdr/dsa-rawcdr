111f79898c9b13ede344acde7ea72f135ea94c62
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-08-21 16:07:15 +0530

    Time: 35 ms (42.96%), Space: 58.9 MB (86.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+
         Arrays.sort(nums);
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Early exit: if the lowest remaining number > 0, sum can't be 0
-            if (nums[i] > 0) break;
+        for (int i = 0; i < n - 2; i++) {
 
-            // Skip duplicate anchor values
+            // Skip duplicate first elements
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1; 
-            int right = nums.length - 1;
+            int left = i + 1;
+            int right = n - 1;
 
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    ans.add(Arrays.asList(
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ));
 
-                    // Skip duplicate left and right values
-                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1])  left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
 
                     left++;
                     right--;
+
                 } else if (sum < 0) {
-                    left++; // Need a larger sum
+                    left++;
                 } else {
-                    right--; // Need a smaller sum
+                    right--;
                 }
             }
         }
 
-        return result;
+        return ans;
     }
 }
