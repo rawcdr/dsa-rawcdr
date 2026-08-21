81e366940dc4d61472666a84078f0e13e4b89421
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-08-21 19:25:25 +0530

    Time: 21 ms (95.7%), Space: 77.7 MB (84.24%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,7 +6,7 @@ class Solution {
         int cnt = 1;
 
         if (nums.length == 0) return 0;
-        if (nums.length == 1) return 1;
+        // if (nums.length == 1) return 1;
 
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == nums[i + 1]) {
