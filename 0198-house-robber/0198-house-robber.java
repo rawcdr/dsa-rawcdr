da7a77b3d213ff57e6069ce422ddbe8acdafe4c6
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-08-21 23:44:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (97.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,29 @@
 class Solution {
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     int dp[] = new int[n + 1];
+
+    //     dp[0] = nums[0];
+    //     dp[1] = nums[1];
+    //     dp[2] = nums[2] + nums[0];
+
+    //     for (int i = 3; i < n; i++) {
+    //         if (i < n) dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i];
+    //         else dp[i] = Math.max(dp[i-2], dp[i-3]);
+    //     }
+
+    //     return dp[n];
+    // }
     public int rob(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n+2];
-
-        for(int i = n-1; i >= 0; i--){
-            int take = nums[i] + dp[i+2];
-            int skip = dp[i+1];
-
-            dp[i] = Math.max(take, skip);
+        int rob=0;
+        int nr=0;
+        for(int i:nums){
+            int t=rob;
+            rob=nr+i;
+            nr=Math.max(t,nr);
         }
-
-        return dp[0];
+        return Math.max(rob,nr);
+       
     }
 }
-
-
-// in this q:
-// this is hardcord:
-// dp recurrnce = max(nums[i] + solve(i+2), solve(i+1));
-// basically take/skip
-// same in tabulation : to store it and use and recursively calc it
