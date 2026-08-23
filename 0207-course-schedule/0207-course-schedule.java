c0ab85cedf0dd2eee910dbec1ac29aa07754e868
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-08-23 21:17:26 +0530

    Time: 4 ms (91.33%), Space: 47.3 MB (33.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // create graph -> add - > init visited and pathvisited
-        List<List<Integer>> graph = new ArrayList<>(); 
+        List<List<Integer>> graph = new ArrayList<>();
 
-        for(int i=0;i<numCourses;i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < numCourses; i++) graph.add(new ArrayList<>());
 
-        for(int[] edge : prerequisites){
-            int a = edge[0];
-            int b = edge[1];
-
-            graph.get(b).add(a);
+        for (int[] a : prerequisites) {
+            graph.get(a[1]).add(a[0]);
         }
 
-        boolean[] visited = new boolean[numCourses];
-        boolean[] pathvisited = new boolean[numCourses];
-
-        for(int i = 0; i < numCourses; i++){
-            if(!visited[i]){
-                if(dfs(i, graph, visited, pathvisited)) return false;
+        int state[] = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, graph, state)) {
+                return false;
             }
         }
 
         return true;
     }
 
-    private boolean dfs(int node, List<List<Integer>> graph, boolean[] visited, boolean[] pathvisited){
-        visited[node] = true;
-        pathvisited[node] = true;
+    private boolean dfs(int c, List<List<Integer>> graph, int[] state) {
+        if (state[c] == 1) return false;
+        if (state[c] == 2) return true;
 
-        for(int next : graph.get(node)){
-            if(!visited[next]){
-                if(dfs(next, graph, visited, pathvisited)) return true;
-            }else if(pathvisited[next]){
-                return true;
-            }
+        state[c] = 1;
+
+        for (int next : graph.get(c)) {
+            if (!dfs(next, graph, state)) return false;
         }
 
-        pathvisited[node] = false;
-        return false;
+        state[c] = 2;
+
+        return true;
     }
 }
