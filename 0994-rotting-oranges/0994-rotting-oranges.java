00a90162fe703f3c6faa0d2e3cc912c29142773d
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-08-23 11:26:43 +0530

    Time: 2 ms (89.58%), Space: 44 MB (72.46%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,49 +3,48 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
         int fresh = 0;
-        int min = 0;
+        Queue<int[]> q = new LinkedList<>();
 
-        int dr[] = {-1,1,0,0};
-        int dc[] = {0,0,-1,1};
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == 2){
-                    q.offer(new int[]{i,j});
-                }else if(grid[i][j] == 1){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
 
-        if(fresh == 0) return 0;
+        if (fresh == 0) return 0;
 
-        while(!q.isEmpty()){
+        int t = 0;
+        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!q.isEmpty() && fresh > 0) {
             int sz = q.size();
 
-            for(int i=0;i<sz;i++){
-                int[] curr = q.poll();
+            for (int i = 0; i < sz; i++){
+                int cell[] = q.poll();
 
-                int row = curr[0];
-                int col = curr[1];
+                int r = cell[0];
+                int c = cell[1];
 
-                for(int k=0;k<4;k++){
-                    int nr = row + dr[k];
-                    int nc = col + dc[k];
+                for (int d[] : dir) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
 
-                    if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == 1){
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
                         grid[nr][nc] = 2;
                         fresh--;
-
-                        q.offer(new int[]{nr,nc});
+                        q.offer(new int[]{nr, nc});
                     }
                 }
             }
-            if(!q.isEmpty()) min++;
+
+            t++;
         }
 
-        return fresh == 0 ? min : -1;
+        return fresh == 0 ? t : -1;
     }
 }
