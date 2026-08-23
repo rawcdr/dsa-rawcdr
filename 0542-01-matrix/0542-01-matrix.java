b8137d5db720a103a2773d1426cb0604d6630b76
Aditya Raj Biswal <141172239+rawcdr@users.noreply.github.com>
2026-08-23 12:26:51 +0530

    Time: 13 ms (91.37%), Space: 48.9 MB (75.43%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,54 +1,35 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-
         int m = mat.length;
         int n = mat[0].length;
 
-        int[][] ans = new int[m][n];
-        boolean[][] visited = new boolean[m][n];
-
         Queue<int[]> q = new LinkedList<>();
-
-        for(int i = 0; i < m; i++){
-
-            for(int j = 0; j < n; j++){
-
-                if(mat[i][j] == 0){
-
-                    q.offer(new int[]{i,j});
-                    visited[i][j] = true;
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) q.offer(new int[]{i, j});
+                else mat[i][j] = -1;
             }
         }
 
-        int[] dr = {-1,1,0,0};
-        int[] dc = {0,0,-1,1};
+        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
+            int cell[] = q.poll();
 
-            int[] curr = q.poll();
+            int r = cell[0];
+            int c = cell[1];
 
-            int r = curr[0];
-            int c = curr[1];
+            for (int d[] : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-            for(int k = 0; k < 4; k++){
-
-                int nr = r + dr[k];
-                int nc = c + dc[k];
-
-                if(nr >= 0 && nr < m &&
-                   nc >= 0 && nc < n &&
-                   !visited[nr][nc]){
-
-                    ans[nr][nc] = ans[r][c] + 1;
-
-                    visited[nr][nc] = true;
-
-                    q.offer(new int[]{nr,nc});
-                }
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == -1) {
+                    mat[nr][nc] = mat[r][c] + 1;
+                    q.offer(new int[]{nr, nc});
+                } 
             }
         }
 
-        return ans;
+        return mat;
     }
 }
